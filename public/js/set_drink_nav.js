e16ee0736641afa7ab9bd2f97642e98e823aecd8
Mark De May <markdemay@TirNaNog.PK5001Z>
2017-04-18 11:13:15 -0600

    Change make link on nav bar match current page.

---
+++ public/js/set_drink_nav.js
@@ -0,0 +1,4 @@
+// Sets the drink link on the nav bar to active
+
+$(".navLink").removeClass("active");
+$("#navDrinks").addClass("active");
---
+++ public/js/set_shop_nav.js
@@ -0,0 +1,4 @@
+// Sets the drink link on the nav bar to active
+
+$(".navLink").removeClass("active");
+$("#navShops").addClass("active");
